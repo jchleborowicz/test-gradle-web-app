0b3bcaf5316ba5c4cd9d8f668509f855cfd897fe
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2018-04-15 14:57:07 +0200

    Web application.

---
+++ src/main/java/org/gradle/demo/HelloServlet.java
@@ -0,0 +1,27 @@
+package org.gradle.demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.apache.commons.lang3.StringUtils.defaultString;
+
+@WebServlet(name = "HelloServlet", urlPatterns = {"hello"}, loadOnStartup = 1)
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println("Hello, World!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final String name = defaultString(req.getParameter("name"), "World");
+
+        req.setAttribute("user", name);
+
+        req.getRequestDispatcher("response.jsp").forward(req, resp);
+    }
+}
